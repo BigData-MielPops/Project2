10c104172ef84e112079175ee8c1385a91e30ac8
Veronica Iovinella <vero.neeja@gmail.com>
2017-07-12 22:04:54 +0200

    mysql import script done

---
+++ Docker/shared-scripts/mysql_import.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, Float, Date, String, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import csv
+import pandas as pd
+import os
+import warnings
+
+#To ignore warnings from already existing mock database
+warnings.filterwarnings('ignore')
+
+Base = declarative_base()
+
+class customers(Base):
+    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+    __tablename__ = 'customers'
+
+    #tell SQLAlchemy the name of column and its attributes: 
+    first_name = Column(VARCHAR(30))
+    last_name = Column(VARCHAR(30))
+    email = Column(VARCHAR(40))
+    income = Column(Integer)
+    ip_address = Column(VARCHAR(30))
+    Id = Column(Integer, primary_key=True, nullable=False, autoincrement=False)
+
+#Create Mysql database if not exists
+
+engine = create_engine('mysql+pymysql://root:password@localhost:3307')
+conn = engine.connect()
+conn.execute("commit")
+conn.execute("create database if not exists mock")
+conn.close()
+
+#Import csv to Mysql
+engine = create_engine('mysql+pymysql://root:password@localhost:3307/mock')
+Base.metadata.create_all(engine)
+path_to_csv = os.path.join(os.path.abspath('../shared-data'),'mock_data_10000_rows_utf8.csv')
+df = pd.read_csv(path_to_csv)
+df.to_sql(con=engine, name='customers', if_exists='replace')
