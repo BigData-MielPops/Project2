fa54dd75b9afcfba1d3693471388bd560ab0d32a
Veronica Iovinella <vero.neeja@gmail.com>
2017-07-12 18:49:20 +0200

    baby steps

--- shared-scripts/sql_import.py
+++ shared-scripts/sql_import.py
@@ -16,7 +16,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import csv
 import pandas as pd
-
+import os
 
 #def Load_Data(file_name):
     #data = csv.reader(file_name, delimiter=',')# skiprows=1, converters={0: lambda s: str(s)})
@@ -35,9 +35,15 @@ class customers(Base):
     income = Column(Integer)
     ip_address = Column(VARCHAR)
     id = Column(Integer, primary_key=True, nullable=False)
+'''
+engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5433')
+conn = engine.connect()
+conn.execute("commit")
+conn.execute("create database if not exists mock")
+conn.close()'''
 
-engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5433/postgres::customers')
+engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5433/mock')
 Base.metadata.create_all(engine)
-path_to_csv = os.path.join(os.path.abspath('shared-data'),'mock_data_10000_rows.csv')
+path_to_csv = os.path.join(os.path.abspath('../Docker/shared-data'),'mock_data_10000_rows.csv')
 df = pd.read_csv(path_to_csv)
 df.to_sql(con=engine, index_label='id', name=customers.__tablename__, if_exists='replace')
