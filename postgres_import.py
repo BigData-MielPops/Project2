803ac02c8c8d10b99c8b818b433859da2d07b6b5
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-07-12 21:50:44 +0200

    postgres import

---
+++ postgres_import.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, Float, Date, String, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import csv
+import pandas as pd
+import os
+
+Base = declarative_base()
+
+class customers(Base):
+    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+    __tablename__ = 'customers'
+
+    #tell SQLAlchemy the name of column and its attributes: 
+    first_name = Column(VARCHAR)
+    last_name = Column(VARCHAR)
+    email = Column(VARCHAR)
+    income = Column(Integer)
+    ip_address = Column(VARCHAR)
+    id = Column(Integer, primary_key=True, nullable=False)
+
+def sql_import(path, verbose=False):
+	#Create Postgres database if not exists
+
+	engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5433')
+	conn = engine.connect()
+	conn.execute("commit")
+
+	value = conn.execute("select count(*) from pg_catalog.pg_database where datname = 'mock' ;").fetchone()
+	if value[0] == 1:
+		if verbose:
+			print ("db already exists")
+	else: 
+		if verbose:
+			print ("creating new db")
+		conn.execute("create database mock")
+	conn.close()
+
+	#Import csv to Postgres
+	engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5433/mock')
+	Base.metadata.create_all(engine)
+	path_to_csv = path
+	df = pd.read_csv(path_to_csv)
+	df.to_sql(con=engine, index_label='id', name=customers.__tablename__, if_exists='replace')
