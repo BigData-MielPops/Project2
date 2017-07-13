c57cc16719642cb99ea4d12bcdd07f5d65bf42b9
Veronica Iovinella <vero.neeja@gmail.com>
2017-07-13 12:13:32 +0200

    memsql import script

---
+++ memsql_import.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, Float, Date, String, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import csv
+import pandas as pd
+import os
+import warnings
+
+#To ignore warnings from already existing mock database
+warnings.filterwarnings('ignore')
+
+Base = declarative_base()
+
+class customers(Base):
+    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+    __tablename__ = 'customers'
+
+    #tell SQLAlchemy the name of column and its attributes: 
+    first_name = Column(VARCHAR(30))
+    last_name = Column(VARCHAR(30))
+    email = Column(VARCHAR(40))
+    income = Column(Integer)
+    ip_address = Column(VARCHAR(30))
+    Id = Column(Integer, primary_key=True, nullable=False, autoincrement=False)
+
+def sql_import(path, verbose=False):
+    #Create Mysql database if not exists
+
+    engine = create_engine('mysql+pymysql://root@localhost:3306')
+    conn = engine.connect()
+    conn.execute("commit")
+    conn.execute("create database if not exists mock")
+    conn.close()
+
+    #Import csv to Mysql
+    engine = create_engine('mysql+pymysql://root@localhost:3306/mock')
+    Base.metadata.create_all(engine)
+    path_to_csv = path
+    df = pd.read_csv(path_to_csv)
+    df.to_sql(con=engine, name='customers', if_exists='replace')
