656bf0e5caea5ec4a8e195765c8806e7c93a5b72
Veronica Iovinella <vero.neeja@gmail.com>
2017-07-13 13:10:08 +0200

    voltdb import commands done

---
+++ Docker/shared-scripts/import_voltdb.sql
@@ -0,0 +1,9 @@
+create table customers(
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(40),
+    income Integer,
+    ip_address VARCHAR(30),
+    Id Integer,
+    PRIMARY KEY (Id)
+);
